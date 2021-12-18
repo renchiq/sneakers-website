1644c8ffee017fa6d39af916a860b0555873f6e2
Test <shebalnikov@mail.ru>
2021-12-18 19:38:52 +0300

    [add] валидация для регистрация и входа

---
+++ scripts/sign-up-validation.js
@@ -0,0 +1,56 @@
+const validationMap = {
+  require: (value) => {
+    return value.trim().length !== 0;
+  },
+};
+
+const parseValidateInputs = () => {
+  const inputs = Array.from(document.querySelectorAll(".input"));
+  const validateObject = inputs.map((input) => {
+    return {
+      element: input,
+      validationFunctions: input.dataset.validation,
+    };
+  });
+  return validateObject;
+};
+
+const setElementStyle = (resultOfValdations) => {
+  resultOfValdations.forEach((result) => {
+    const { element, isValid } = result;
+    const isElementValid = element.classList.contains("bad-validation");
+    if (isElementValid) {
+      if (isValid) {
+        element.classList.remove("bad-validation");
+      }
+    } else {
+      if (!isValid) {
+        element.classList.add("bad-validation");
+      }
+    }
+  });
+};
+
+const signUpHandler = (event) => {
+  event.preventDefault();
+  const validateObjectOfInputs = parseValidateInputs();
+  const resultOfValidation = validateObjectOfInputs.map(
+    ({ element, validationFunctions }) => {
+      const result = {
+        isValid: true,
+        element,
+        validationFunctions,
+      };
+      const needValidation = validationFunctions?.split(" ");
+      const { value: inputValue } = element;
+      needValidation?.forEach((validFn) => {
+        result.isValid = result.isValid && validationMap[validFn](inputValue);
+      });
+      return result;
+    }
+  );
+  setElementStyle(resultOfValidation);
+};
+
+const signUpButton = document.querySelector(".sign-up__send-button");
+signUpButton.addEventListener("click", signUpHandler);
